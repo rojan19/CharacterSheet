68cf0a10e793b81acef755a08c883337e7eb892d
janda <janda@fio.cz>
2019-05-17 10:24:37 +0200

    Revert "images"
    This reverts commit c987a9aabb29c94e4ecf9d4beae417ecfee75c4a.
    
    5F

--- src/components/ChooseCharacter.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by janda on 25.4.2019.
- */
-import CharacterDataService from '../service/CharacterDataService'
-import React, { Component } from 'react';
-export default class ChooseCharacter extends Component {
-
-}
